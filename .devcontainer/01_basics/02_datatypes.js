f0ff0567ecfd0c98fca43f3af48f16e6cf7cba38
MAhsan89 <152721796+MAhsan89@users.noreply.github.com>
2024-03-04 13:43:22 +0000

    container config done

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,10 @@
+"use strict"; // Treat all JS Code as a newer version
+
+// alert ( 3 + 3 )  //we are using node.js not browser 
+
+/* doc  "mdn" not an orgional documentation butfrom Mozilla, however easy to understand. 
+origional documentation for JS is "tc39.es" ECMA Script
+
+*/
+
+Let
